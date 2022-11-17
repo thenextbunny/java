8cbc6a78c79ab923096ed023dd073122735c4c51
Vitor <vitorcoelhosilvat@gmail.com>
2022-11-17 13:46:18 -0300

    Initial commit

---
+++ college/variables.java
@@ -0,0 +1,19 @@
+// Name of the classe
+class Variables {
+    // Main method with String array as argument
+    public static void main(String args[]) {
+        // Variable declaration
+        int age = 19;
+        String name = "Coelho"
+        double height = 173.0;
+        boolean student = true;
+
+        // Print the variables
+        System.out.println("My name is " + name + " and I'm " age, " years old.");
+        System.out.println("My height is ", height, "cm.");
+        System.out.println("It's ", student, " that I'm a student.");
+
+        // Terminate the program
+        System.exit(0);
+    }
+}
